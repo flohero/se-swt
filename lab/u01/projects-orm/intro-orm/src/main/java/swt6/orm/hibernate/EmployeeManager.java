34b22fae80682b86f3844b60bdf8aad4c76be0fd
florian weingartshofer <florian.weingartshofer@tuta.io>
2022-03-01 13:44:01 +0100

    add u01 part 2

---
+++ lab/u01/projects-orm/intro-orm/src/main/java/swt6/orm/hibernate/EmployeeManager.java
@@ -0,0 +1,88 @@
+package swt6.orm.hibernate;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import swt6.orm.domain.Employee;
+import swt6.util.HibernateUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class EmployeeManager {
+
+    static String promptFor(BufferedReader in, String p) {
+        System.out.print(p + "> ");
+        System.out.flush();
+        try {
+            return in.readLine();
+        } catch (Exception e) {
+            return promptFor(in, p);
+        }
+    }
+
+    private static void saveEmployeeOld(Employee employee) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(employee);
+        tx.commit();
+
+        session.close();
+        sessionFactory.close();
+    }
+
+    private static void saveEmployee(Employee employee) {
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.save(employee);
+        tx.commit();
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String availCmds = "commands: quit, insert";
+        HibernateUtil.getCurrentSession();
+
+        System.out.println("Hibernate Employee Admin");
+        System.out.println(availCmds);
+        String userCmd = promptFor(in, "");
+
+        try {
+
+            while (!userCmd.equals("quit")) {
+
+                switch (userCmd) {
+
+                    case "insert":
+                        saveEmployee(new Employee(
+                                promptFor(in, "first name"),
+                                promptFor(in, "last name"),
+                                LocalDate.parse(promptFor(in, "Date of Birth"), formatter)
+                        ));
+                        break;
+
+                    default:
+                        System.out.println("ERROR: invalid command");
+                        break;
+                }
+
+                System.out.println(availCmds);
+                userCmd = promptFor(in, "");
+            } // while
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            HibernateUtil.closeSessionFactory();
+        }
+    }
+}
---
+++ lab/u01/projects-orm/intro-orm/src/main/java/swt6/util/HibernateUtil.java
@@ -0,0 +1,30 @@
+package swt6.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) {
+            sessionFactory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+    public static void closeSessionFactory() {
+        if(sessionFactory != null) {
+            sessionFactory.close();
+            sessionFactory = null;
+        }
+    }
+
+    public static Session getCurrentSession() {
+        return getSessionFactory().getCurrentSession();
+    }
+}
